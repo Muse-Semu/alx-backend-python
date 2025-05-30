13449cd5ab7d468d7f850d5ad5d7137edd1c0703
mama <smuse1756@gmail.com>
2025-05-31 01:50:13 +0300

    add api-auth in url

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),
+    path('api/', include('chats.urls')),  # Include chats app routes for conversations and messages
+    path('api-auth/', include('rest_framework.urls')),  # Include DRF authentication endpoints
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
