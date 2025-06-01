6165b73db61da222d8f3e1b6dbf723b049886312
mama <smuse1756@gmail.com>
2025-06-01 17:54:30 +0300

    remove white spaces from end line

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -113,4 +113,3 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
             repos_url = self.org_payload["repos_url"]
             mock_get.assert_any_call(org_url)
             mock_get.assert_any_call(repos_url)
-          
