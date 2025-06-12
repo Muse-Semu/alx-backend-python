659befb47adf6fb409f92ecf96ef4086aaab901c
mama <smuse1756@gmail.com>
2025-06-12 12:55:00 +0300

    create the old message as history befor edited

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -9,3 +9,25 @@ def create_message_notification(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+
+
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    """
+    Logs message edits by comparing old and new content.
+    Creates a MessageHistory record when content changes.
+    """
+    if instance.pk:  # Only for existing messages (edits)
+        try:
+            old_message = Message.objects.get(pk=instance.pk)
+            if old_message.content != instance.content:  # Content changed
+                MessageHistory.objects.create(
+                    original_message=instance,
+                    old_content=old_message.content,
+                    edited_by=instance.sender
+                )
+                instance.edited = True
+                instance.last_edited = timezone.now()
+        except Message.DoesNotExist:
+            pass  # New message being created
